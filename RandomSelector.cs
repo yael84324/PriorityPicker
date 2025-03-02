fa3caaf2ba7e768ccf4f3700a64e84d895d44d8b
Yael Katzanelenbogen <yael84324@gmail.com>
2025-03-02 10:12:28 +0200

    Enhance code with comments and error handling

--- RandomSelector.cs
+++ RandomSelector.cs
@@ -45,8 +45,12 @@ public class RandomSelector
 
         foreach (var (key, priorityInfo) in priorityInfos)
         {
+            // Calculate the probability of the current priority group.
+            // The probability is calculated as the number of items in the group times the probability of a single item, divided by the total probability.
             double groupWeight = (double)(priorityInfo.NumberOfItems * priorityInfo.ProbabilityForSingleItem / totalProbability);
 
+            // Checks whether the chosen random number is within the probability range of the current priority group.
+            // If so, this means that an item from this priority group should be chosen and remove. 
             if (selectedValue <= accumulatedWeight + groupWeight)
             {
                 var selectedItem = GetAndRemoveItemWithPriority(priorityInfo, key);
@@ -56,29 +60,34 @@ public class RandomSelector
             accumulatedWeight += groupWeight;
         }
 
-        return null;
+        throw new InvalidOperationException("Failed to select an item.");
     }
 
     private Item GetAndRemoveItemWithPriority(PriorityInfo priorityInfo, int key)
     {
-        if (priorityInfo.NumberOfItems > 0)
+        if (priorityInfo.NumberOfItems <= 0)
         {
-            var selectedItem = items
-                .Where(item => item.Priority == key)
-                .OrderBy(x => rand.Next())
-                .FirstOrDefault();
+            throw new InvalidOperationException("Cannot select an item from an empty priority group.");
+        }
 
-            if (selectedItem != null)
-            {
+        var selectedItem = items
+            .Where(item => item.Priority == key)
+            .OrderBy(x => rand.Next())
+            .FirstOrDefault();
 
-                items.Remove(selectedItem);
+        if (selectedItem != null)
+        {
 
-                priorityInfo.DeleteItem();
+            items.Remove(selectedItem);
 
-                totalProbability -= priorityInfo.ProbabilityForSingleItem;
+            // decrease number of items for the selected item priority
+            priorityInfo.DeleteItem();
 
-                return selectedItem;
-            }
+            // Update the total chance after removing an item
+            // The total chance decreases by the chance of a single item from the selected group
+            totalProbability -= priorityInfo.ProbabilityForSingleItem;
+
+            return selectedItem;
         }
 
         return null;
